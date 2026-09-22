a3cc79eda4a2d59fc258fdf290fbac2f5113e269
root <root@ip-192-168-1-26.ap-northeast-2.compute.internal>
2026-09-22 08:53:48 +0000

    fix(teamcity): remove invalid package declaration in Kotlin DSL settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,3 @@
-package .teamcity
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
